50843538e120c41e900a14d6f60abf7fcd9beaee
Santiago L. <s.lasso@uniandes.edu.co>
2025-09-28 11:19:31 -0500

    Agregado log de eventos al servicio autorizador

--- autorizador/app.py
+++ autorizador/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_jwt_extended import create_access_token, JWTManager
 import datetime
 import os
+import json
 
 app = Flask(__name__)
 
@@ -19,7 +20,25 @@ AUTHORIZED_USERS = {
     }
 }
 
+LOG_FILE = "/app/event_log.txt"
+
+def writeLog(event_type, source, message, details=None):
+    timestamp = datetime.datetime.now().isoformat()
+    log = {
+        "timestamp": timestamp,
+        "type": event_type,
+        "source": source,
+        "message": message,
+        "details": details if details else {}
+    }
+    try:
+        with open(LOG_FILE, 'a') as file:
+            file.write(json.dumps(log) + '\n')
+    except Exception as e:
+        print(f"ERROR ESCRITURA: No fue posible escribir en el archivo del log {LOG_FILE}: {e}", flush=True)
+
 def notify_admin(username):
+    writeLog("ALERT", "Autorizador", "Intento de inicio de sesion fallido", f"Intento realizado por el ususario {username}")
     print(f"ALERTA DE SEGURIDAD: Intento de inicio de sesión fallido para el usuario '{username}'.",flush=True)
 
 @app.route('/login', methods=['POST'])
@@ -36,6 +55,7 @@ def login():
             additional_claims={"role": user["role"]},
             expires_delta=datetime.timedelta(minutes=15)
         )
+        writeLog("AUDIT", "Autorizador", "Token de acceso concedido", f"Token concedido al usuario {username}")
         return jsonify(access_token=access_token), 200
     else:
         notify_admin(username)
